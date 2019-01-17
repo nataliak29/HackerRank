9abec725073886a2c8bac1b9da1c446906c7ab96
nataliak29 <n.a.kukushkina@gmail.com>
2019-01-17 11:08:51 -0500

    Update birthdaycake.py

--- birthdaycake.py
+++ birthdaycake.py
@@ -1,11 +1,13 @@
+#https://www.hackerrank.com/challenges/birthday-cake-candles/problem
 ar_count=4
 ar=[3,2,1,3]
 
-maxheight=max(ar)
-candles=0
+def birthdayCakeCandles(ar):
+    maxheight=max(ar)
+    candles=0
 
-for i in range(ar_count):
-    if ar[i]==maxheight:
-        candles+=1
+    for i in range(ar_count):
+        if ar[i]==maxheight:
+            candles+=1
 
-print(candles)
+    return candles
